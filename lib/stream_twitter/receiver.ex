d3bde9849d064aa7874192345e76dafbf45fde30
sohjiro <felipe@makingdevs.com>
2016-11-13 12:39:03 -0600

    using GenServer behaviour in streamer... WIP

--- lib/stream_twitter/receiver.ex
+++ lib/stream_twitter/receiver.ex
@@ -3,11 +3,13 @@ defmodule StreamTwitter.Receiver do
   def start(counter, tweets) do
     receive do
       {:start} ->
-        pid = spawn(StreamTwitter.Streamer, :stream, [])
-        send(pid, {:start, "FelizSabado", self})
+        IO.puts "starting gen server"
+        {:ok, pid} = StreamTwitter.Streamer.start_link("FelizDomingo")
+        StreamTwitter.Streamer.start_stream(pid, self)
         start(counter, tweets)
       {:incoming, tweet} -> start(counter + 1, [tweet | tweets])
       {:tweets, pid} ->
+        # GenServer.call(pid, tweets)
         send(pid, tweets)
         start(counter + 1, tweets)
       {:count} ->
--- lib/stream_twitter/streamer.ex
+++ lib/stream_twitter/streamer.ex
@@ -1,13 +1,19 @@
 defmodule StreamTwitter.Streamer do
+  use GenServer
 
-  def stream do
-    receive do
-      {:start, text, pid} ->
-        IO.puts "starting"
-        stream = ExTwitter.stream_filter(track: text)
-        for tweet <- stream do
-          send(pid, {:incoming, tweet.text})
-        end
+  def start_link(text) do
+    stream = ExTwitter.stream_filter(track: text)
+    GenServer.start_link(__MODULE__, stream)
+  end
+
+  def start_stream(pid, response_to) do
+    GenServer.cast(pid, {:start, response_to})
+  end
+
+  def handle_cast({:start, pid}, stream) do
+    for tweet <- stream do
+      send(pid, {:incoming, tweet.text})
+      # GenServer.call(pid, {:incoming, tweet.text})
     end
   end
 
