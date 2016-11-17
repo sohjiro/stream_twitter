c19d42f85b08298f00ac2c25a0062aa0a2d7971a
Sohjiro <felipe@makingdevs.com>
2016-11-17 12:28:21 -0600

    adding application behaviour

--- lib/stream_twitter.ex
+++ lib/stream_twitter.ex
@@ -1,2 +1,19 @@
 defmodule StreamTwitter do
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      worker(StreamTwitter.Receiver, [])
+    ]
+
+    opts = [strategy: :simple_one_for_one, name: StreamTwitter.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  def search(name, text), do: Supervisor.start_child(StreamTwitter.Supervisor, [name, text])
+  def total(name), do: StreamTwitter.Receiver.total(name)
+  def tweets(name), do: StreamTwitter.Receiver.tweets(name)
+
 end
