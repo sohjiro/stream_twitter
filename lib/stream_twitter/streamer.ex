022d3191feb515fc88eb6033f09ea46a3f4b483f
Sohjiro <felipe@makingdevs.com>
2016-11-18 12:25:07 -0600

    adding ets... WIP

--- lib/stream_twitter/streamer.ex
+++ lib/stream_twitter/streamer.ex
@@ -3,18 +3,26 @@ defmodule StreamTwitter.Streamer do
 
   def start_link(name, text) do
     IO.puts "name : #{inspect name} with text: #{text}"
+    GenServer.start_link(__MODULE__, {name, text})
+  end
+
+  def init({name, text}) do
     stream = ExTwitter.stream_filter(track: text)
-    GenServer.start_link(__MODULE__, stream)
+    create_ets(name)
+    {:ok, {stream, name}}
   end
 
   def start_stream(pid, response_to) do
     GenServer.cast(pid, {:start, response_to})
   end
 
-  def handle_cast({:start, pid}, stream) do
+  def handle_cast({:start, _pid}, {stream, name}) do
     for tweet <- stream do
-      GenServer.cast(pid, {:incoming, tweet.text})
+      :ets.insert(name, {"tweets", tweet.text})
     end
+    {:noreply, {stream, name}}
   end
+
+  defp create_ets(name), do: :ets.new(name, [:bag, :protected, :named_table])
 
 end
