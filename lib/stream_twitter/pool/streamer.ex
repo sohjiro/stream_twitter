ce79585012e5437ecb4d30b88436793c9dac8ebc
sohjiro <felipe@makingdevs.com>
2016-11-21 21:04:40 -0600

    saving tweet with DataAccess help

--- lib/stream_twitter/pool/streamer.ex
+++ lib/stream_twitter/pool/streamer.ex
@@ -1,9 +1,8 @@
 defmodule StreamTwitter.Pool.Streamer do
 
-  def stream(name, text) do
-    StreamTwitter.DataAccess.init(name)
+  def stream(text) do
     stream = ExTwitter.stream_filter(track: text)
-    for tweet <- stream, do: StreamTwitter.DataAccess.create(name, {tweet.text, tweet.created_at})
+    for tweet <- stream, do: StreamTwitter.DataAccess.create(text, {tweet.text, tweet.created_at})
   end
 
 end
